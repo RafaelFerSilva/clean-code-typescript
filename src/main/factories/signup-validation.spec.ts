75551779cafeb789355de1b6ccfbc1bd68e7993c
Rafael Silva <todosomtodavoz@gmail.com>
2023-08-01 07:13:22 -0300

    refactor: rename class

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,5 +1,5 @@
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
-import { RequiredFieldValidarion } from '../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { type Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
@@ -11,7 +11,7 @@ describe('SignupValidation Factory', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-      validations.push(new RequiredFieldValidarion(field))
+      validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
